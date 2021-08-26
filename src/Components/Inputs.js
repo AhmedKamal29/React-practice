5a584c5eb5eecde8119f4c71a6aacfb6e3be0226
AhmedKamal29 <ahmed.kamal29@outlook.com>
2021-08-26 11:31:31 +0200

    Added the mic icon inside the input  field

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -1,21 +1,34 @@
+import React from "react";
 import PropTypes from "prop-types";
-import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import Button from "./Button";
 import { BsFillMicFill } from "react-icons/bs";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
 
 const Inputs = ({ placeholder }) => {
   const onClick = () => {
     console.log("adding a task");
   };
+
   return (
     <div className="TextArea">
-      <TextField
-        id="outlined-basic"
-        label="What is your task"
-        variant="outlined"
-      >
-        <BsFillMicFill className="micIcon" />
-      </TextField>
+      <FormControl variant="outlined">
+        <InputLabel className="inputLabel">What is your task</InputLabel>
+        <OutlinedInput
+          label="what is your task"
+          endAdornment={
+            <InputAdornment position="end">
+              <BsFillMicFill
+                aria-label="toggle password visibility"
+                className="micIcon"
+                edge="end"
+              />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
       <Button text="Add Task" onClick={onClick} />
     </div>
   );
